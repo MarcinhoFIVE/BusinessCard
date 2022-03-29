5e865d18cb0b2eaf1969502ea74344dd8f67984a
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-03-29 09:15:32 -0300

    Finalizacao Adapter

--- app/src/main/java/br/mdan/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/mdan/businesscard/ui/MainActivity.kt
@@ -2,16 +2,26 @@ package br.mdan.businesscard.ui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import br.mdan.businesscard.App
 import br.mdan.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+    private val adapter by lazy { BusinessCardAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListeners()
     }
 
@@ -20,6 +30,12 @@ class MainActivity : AppCompatActivity() {
             val Intent = Intent(this@MainActivity, AddBusinesCardActivity::class.java)
             startActivity(Intent)
 
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCard ->
+            adapter.submitList(businessCard)
         }
     }
 }
